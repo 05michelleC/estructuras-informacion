3c434089a0de6cac7bfdf1c944f1bf2cf9b2a572
A <A@DESKTOP-0S4IPV8>
2021-11-24 17:17:46 -0500

    TallerVideoArbol

---
+++ src/arbol/NodoArbol.java
@@ -0,0 +1,16 @@
+package arbol;
+
+public class NodoArbol {
+
+    Persona dato;
+    NodoArbol izq, der;
+
+    public NodoArbol(Persona d) {
+        dato = d;
+        izq = der = null;
+    }
+
+    public String toString() {
+        return "el dato es:" + dato;
+    }
+}
